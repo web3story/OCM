81851ac6ef0f49ac070cddbaf68d273cda7a986b
web3story <web3story@users.noreply.github.com>
2022-10-19 23:05:48 +0000

    deploy: 1ae04815cb8029dc29bf3157d4120632b73de43c

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+
+document.querySelectorAll(".card").forEach(c=>{
+    c.addEventListener("click",(e)=>{
+        document.getElementById("modal-window").classList.remove("hidden");
+        // get the content into the div
+        const content = e.currentTarget.querySelector(".card-content").innerHTML;
+        document.getElementById("modal-content").innerHTML = content;
+        // get the title into the div
+        const title = e.currentTarget.querySelector(".card-title").innerHTML;
+        document.getElementById("modal-title").innerHTML = title;
+
+    })
+})
+
+document.getElementById("modal-close").addEventListener("click",(e)=>{
+    document.getElementById("modal-window").classList.add("hidden");
+})
